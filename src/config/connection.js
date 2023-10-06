de7676a1293cc737a8099bfa3f2307bb25bf2e7c
irfanakbari <irfanakbarihabibi18@gmail.com>
2023-10-06 11:16:18 +0700

    fix sequelize connection bug

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,8 +1,11 @@
 import {Sequelize} from "sequelize";
+import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack
+
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2
 });
 
 
